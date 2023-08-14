1012e76832ff637e37dca365a63e7e891bc68bfd
Kenichiro Hara <khara@newrelic.com>
2023-08-14 16:18:45 +0900

    乱数を計算する関数を追加

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const port = 3000,
     express = require("express"),
     app = express();
 
+//乱数生成を生成するための関数
+function getRandomVal(max){
+    return Math.floor(Math.random()*max);
+}
+
+
 app.get("/", (req, res) => {
-    res.send("Hello, Universe!");
+
+    let retStatusIndex = getRandomVal(100);
+    if( retStatusIndex < 70 ){
+        res.send("New Relic!");
+    }else{
+        res.status(404).send("Hoge..");
+    }
 })
 .listen(port, () => {
     console.log('The Express.js server has started and is listening on port number:' + ` ${port}`);
